ec4b155f30dcc7780f801f5461f2245cd6f17973
danielccosta9 <64346724+danielccosta9@users.noreply.github.com>
2021-03-24 08:56:03 -0300

    Update Semana05_Q1.js
    Version final

--- Semana05_Q1.js
+++ Semana05_Q1.js
@@ -1,19 +1,16 @@
-let qtd = 0;
-function myFunc(mensagem) {
-	qtd++
-	//Para fazer a encrementação
+let numLista = window.prompt("Digite a quantindade de lista: ")//Pedindo a quantidade de lista que será criada.
+let texto = window.prompt("Digite um nome: ")//Pedindo que irá estar dentro da lista.
 
-	console.log(mensagem, `Lista${qtd}`);
-	document.getElementById("myList").appendChild(mensagem);
-	//Adcionando uma lista no documento com ID "myList"
-}
+let qtd = 0; //Criando o incrementador.
+function myFunction() {
+    qtd++ //Sempre que a função for chamada, irá incrementar auqi.
 
-let valor = window.prompt("Digite a quantidade de listas: ");
-//Para pedir ao usauario que digite um valor
+    let criarElemento = document.createElement("LI"); //Fazendo a criacao da tag "<li> </li>" 
+    let textoLista = document.createTextNode(`${texto} ${qtd}`);//Adicionando o texto digitado pelo o usuario e a numeracao da quantidade da lista ao lado do texto.
+    criarElemento.appendChild(textoLista);
+    document.getElementById("myList").appendChild(criarElemento);//Informando o local que irá ficar a lista.
+};
 
-let creatList = document.createElement("LI");
-//Criando uma lista dentro do documento
-
-for(let i = 0; i < valor; i++) {
-	myFunc(creatList)
+for (let i = 0; i < numLista; i++){
+    myFunction();
 };
