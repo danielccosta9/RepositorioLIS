9911cbe2f15a096e3d1e9d2689c1ddef39510802
danielccosta9 <danielccosta9@github.com>
2021-04-19 20:08:17 -0300

    100% IMC

--- IMC/imc.js
+++ IMC/imc.js
@@ -14,57 +14,54 @@ function imc() {
 
         let valorIMC = (peso / (altura * altura)).toFixed(1); //Calculo para o IMC.
 
-        //Homem
-        let homem = document.getElementById('masculino').checked = true;
-        let mulher = document.getElementById('feminino').checked = true;
+        
+        let sexo = document.getElementsByName('sexo');//Buscando o Sexo do User.
 
-        if (homem){ //Caso seja Homem
-            console.log('Masculino selec')
+        if (sexo[0].checked){ //Caso seja Homem
             if (valorIMC <= 19) {
-                resultado.textContent = 'Abaixo do Peso';//Texto da situação.
+                resultado.textContent = 'Abaixo do Peso';//Texto da Condicao.
                 document.querySelector('#resultado').style.background = "yellow";//Cor do Background.
-                document.querySelector('#resultado').style.color = "black";//Cor do texto diferente dos demais.
+                
             }
             else if (valorIMC <= 25.7){
-                resultado.textContent = 'Peso Normal';//Texto da situação.
+                resultado.textContent = 'Peso Normal';//Texto da Condicao.
                 document.querySelector('#resultado').style.background = "green";//Cor do Background.
             }
             else if (valorIMC <= 27.3){
-                resultado.textContent = 'Marginalmente Acima do Peso';//Texto da situação.
-                document.querySelector('#resultado').style.background = "peru";//Cor do Background.
+                resultado.textContent = 'Marginalmente Acima do Peso';//Texto da Condicao.
+                document.querySelector('#resultado').style.background = "yellow";//Cor do Background.
             }
             else if (valorIMC <= 32.3){
-                resultado.textContent = 'Acima do Peso Ideal';//Texto da situação.
-                document.querySelector('#resultado').style.background = "orange";//Cor do Background.
+                resultado.textContent = 'Acima do Peso Ideal';//Texto da Condicao.
+                 document.querySelector('#resultado').style.background = "yellow";//Cor do Background.
             }
             else {
-                resultado.textContent = 'Obeso';//Texto da situação.
+                resultado.textContent = 'Obeso';//Texto da Condicao.
                 document.querySelector('#resultado').style.background = "red";//Cor do Background.
             }
         }
 
-        //Mulher
-        else{
-            console.log('Mulher selec')
+        
+        else{ //Mulher
             if (valorIMC <= 19) {
-                resultado.textContent = 'Abaixo do Peso';//Texto da situação.
+                resultado.textContent = 'Abaixo do Peso';//Texto da Condicao.
                 document.querySelector('#resultado').style.background = "yellow";//Cor do Background.
-                document.querySelector('#resultado').style.color = "black";//Cor do texto diferente dos demais.
+                
             }
             else if (valorIMC <= 25.7){
-                resultado.textContent = 'Peso Normal';//Texto da situação.
+                resultado.textContent = 'Peso Normal';//Texto da Condicao.
                 document.querySelector('#resultado').style.background = "green";//Cor do Background.
             }
             else if (valorIMC <= 27.3){
-                resultado.textContent = 'Marginalmente Acima do Peso';//Texto da situação.
-                document.querySelector('#resultado').style.background = "peru";//Cor do Background.
+                resultado.textContent = 'Marginalmente Acima do Peso';//Texto da Condicao.
+                document.querySelector('#resultado').style.background = "yellow";//Cor do Background.
             }
             else if (valorIMC <= 32.3){
-                resultado.textContent = 'Acima do Peso Ideal';//Texto da situação.
-                document.querySelector('#resultado').style.background = "orange";//Cor do Background.
+                resultado.textContent = 'Acima do Peso Ideal';//Texto da Condicao.
+                 document.querySelector('#resultado').style.background = "yellow";//Cor do Background.
             }
             else {
-                resultado.textContent = 'Obeso';//Texto da situação.
+                resultado.textContent = 'Obeso';//Texto da Condicao.
                 document.querySelector('#resultado').style.background = "red";//Cor do Background.
             } 
         }
